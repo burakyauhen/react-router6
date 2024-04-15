3630c66fe93ccf0698357b69fe735f22aadddb2f
Burak Yauhen <burakyauhen@gmail.com>
2024-04-15 11:21:41 +0300

    feat: add params of searching

--- src/pages/Blogpage.jsx
+++ src/pages/Blogpage.jsx
@@ -1,8 +1,17 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
+import { BlogFilter } from "../components/BlogFilter";
 
 const Blogpage = () => {
     const [posts, setPosts] = useState([]);
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const postQuery = searchParams.get('post') || '';
+    const latest = searchParams.has('latest');
+
+    const startsFrom = latest ? 80 : 1;
+
+    
 
     useEffect(() => {
         const getPosts = async () => {
@@ -15,11 +24,19 @@ const Blogpage = () => {
        
 
     }, []);
+    
     return (
         <div>
             <h1>Our news</h1>
+            <BlogFilter 
+                setSearchParams={setSearchParams}
+                postQuery={postQuery}
+                latest={latest}
+            
+            />
             <Link to="/posts/new">Add new post</Link>
-            {posts.map(post => (
+            {posts.filter(post => post.title.includes(postQuery) && post.id >= startsFrom
+            ).map(post => (
                 <Link key={post.id} to={`/posts/${post.id}`}>
                     <li>{post.title}</li>
                 </Link>
